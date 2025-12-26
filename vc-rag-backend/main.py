193a37ba2a7c9cb021615c1384dcafa169dad573
TheAmateurCoder2 <vismayhi@gmail.com>
2025-12-26 17:55:50 +0000

    enable cors for rag

---
+++ vc-rag-backend/main.py
@@ -0,0 +1,27 @@
+# DEPLOY TEST CORS ENABLED
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # allow Firebase hosting
+    allow_credentials=True,
+    allow_methods=["*"],   # allow POST, OPTIONS
+    allow_headers=["*"],   # allow Content-Type
+)
+
+
+class Query(BaseModel):
+    query: str
+
+@app.post("/rag")
+async def rag(q: Query):
+    return {
+        "answer": f"AI result for: {q.query}"
+    }
